28d697d92d38d226e20355716c9343a221653e12
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2022-05-06 19:37:13 +0700

    fix bugs

--- netlify/functions/telegram_bot.js
+++ netlify/functions/telegram_bot.js
@@ -2,6 +2,6 @@ const sendVideoService = require("../../utils/sendVideoService");
 
 exports.handler = async (event) => {
     const { message } = JSON.parse(event.body);
-    await sendMessage(message.chat.id, "I got your message!");
+    await sendVideoService(message.chat.id, "I got your message!");
     return { statusCode: 200 };
 };
