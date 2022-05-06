4a737384bb9a1569df9fde734f7ad298e436248b
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2022-05-06 19:32:19 +0700

    testing

---
+++ utils/sendVideoService.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const axios = require("axios").default;
+
+module.exports = async (chat_id, text) => {
+  await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
+    chat_id,
+    text,
+  });
+
+  return true;
+};
