04d8edb6d3ba81eb95c674003925011b6a82430f
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2022-05-06 20:08:24 +0700

    log message

--- netlify/functions/telegram_bot.js
+++ netlify/functions/telegram_bot.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
+
 const sendVideoService = require("../../utils/sendVideoService");
+
+video_url = "https://vm.tiktok.com/ZSeQS6B5k/";
 
 exports.handler = async (event) => {
     const { message } = JSON.parse(event.body);
     await sendVideoService(message.chat.id, "I got your message!");
+
+    console.log(message);
+
     return { statusCode: 200 };
 };
--- utils/sendVideoService.js
+++ utils/sendVideoService.js
@@ -1,12 +1,35 @@
-require('dotenv').config()
-
 const axios = require("axios").default;
 
-module.exports = async (chat_id, text) => {
-  await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
-    chat_id,
-    text,
-  });
+module.exports = async ({ chat_id, video_url }) => {
+    // getting video url
+    const options = {
+        method: "GET",
+        url: "https://tiktok-video-no-watermark2.p.rapidapi.com/",
+        params: { url: video_url, hd: "0" },
+        headers: {
+            "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com",
+            "X-RapidAPI-Key": process.env.RAPID_API_TOKEN,
+        },
+    };
 
-  return true;
+    const url = await axios
+        .request(options)
+        .then(function (response) {
+            return response.data.data.play;
+        })
+        .catch(function (error) {
+            console.error(error);
+            return "";
+        });
+
+    if (url == "") return false;
+    await axios.post(
+        `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendVideo`,
+        {
+            chat_id,
+            video: video_url,
+        }
+    );
+
+    return true;
 };
