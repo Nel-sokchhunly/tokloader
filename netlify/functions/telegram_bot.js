52fecf114cf7d926a578d9d31b5fa12b5afb80a8
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2022-05-06 22:42:18 +0700

    fix bugs

--- netlify/functions/telegram_bot.js
+++ netlify/functions/telegram_bot.js
@@ -6,33 +6,35 @@ const sendMessage = require("../../utils/sendMessage");
 exports.handler = async (event) => {
     const { message } = JSON.parse(event.body);
 
-    if (message.text == undefined) return { statusbar: 500 };
+    if (message && message.text) {
+        var TEXT = message.text;
 
-    var TEXT = message.text;
-
-    const re = new RegExp(
-        "^(https://vm.tiktok.com/)|(https://vt.tiktok.com/).*$"
-    );
-    const isTikTokVideo = re.test(TEXT);
-
-    if (TEXT == "/start") {
-        await sendMessage(
-            message.chat.id,
-            "Welcome to the bot. Share me tiktok url to start downloading video!"
+        const re = new RegExp(
+            "^(https://vm.tiktok.com/)|(https://vt.tiktok.com/).*$"
         );
-        return { statusCode: 200 };
-    } else if (!isTikTokVideo) {
-        await sendMessage(
-            message.chat.id,
-            "the text you sent is not tiktok url"
-        );
-        return { statusCode: 200 };
+        const isTikTokVideo = re.test(TEXT);
+
+        if (TEXT == "/start") {
+            await sendMessage(
+                message.chat.id,
+                "Welcome to the bot. Share me tiktok url to start downloading video!"
+            );
+            return { statusCode: 200 };
+        } else if (!isTikTokVideo) {
+            await sendMessage(
+                message.chat.id,
+                "the text you sent is not tiktok url"
+            );
+            return { statusCode: 200 };
+        } else {
+            await sendMessage(message.chat.id, "Getting you the video...");
+            sendVideoService({
+                chat_id: message.chat.id,
+                video_url: TEXT,
+            });
+            return { statusCode: 200 };
+        }
     } else {
-        await sendMessage(message.chat.id, "Getting you the video...");
-        sendVideoService({
-            chat_id: message.chat.id,
-            video_url: TEXT,
-        });
-        return { statusCode: 200 };
+        console.log("text is undefined");
     }
 };
