532c145658844b631e9678f9dd96ce8ce60b69e9
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2022-05-06 21:31:00 +0700

    add start message

--- netlify/functions/telegram_bot.js
+++ netlify/functions/telegram_bot.js
@@ -6,23 +6,30 @@ const sendMessage = require("../../utils/sendMessage");
 exports.handler = async (event) => {
     const { message } = JSON.parse(event.body);
 
-    // regex
-    const re = new RegExp(
-        "^(https://vm.tiktok.com/)|(https://vt.tiktok.com/).*$"
-    );
-    const isTikTokVideo = re.test(message.text);
-
-    if (!isTikTokVideo) {
+    if (message.text == "/start") {
         await sendMessage(
             message.chat.id,
-            "the text you sent is not tiktok url"
+            "Welcome to the bot. Share me tiktok url to start downloading video!"
         );
     } else {
-        await sendMessage(message.chat.id, "Getting you the video...");
-        await sendVideoService({
-            chat_id: message.chat.id,
-            video_url: message.text.trim(),
-        });
+        // regex
+        const re = new RegExp(
+            "^(https://vm.tiktok.com/)|(https://vt.tiktok.com/).*$"
+        );
+        const isTikTokVideo = re.test(message.text);
+
+        if (!isTikTokVideo) {
+            await sendMessage(
+                message.chat.id,
+                "the text you sent is not tiktok url"
+            );
+        } else {
+            await sendMessage(message.chat.id, "Getting you the video...");
+            await sendVideoService({
+                chat_id: message.chat.id,
+                video_url: message.text.trim(),
+            });
+        }
     }
 
     return { statusCode: 200 };
